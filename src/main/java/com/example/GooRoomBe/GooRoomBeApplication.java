b9d597d911176a5f9c41a0b031723c5ae6dea8db
Dcaffein <hj02117@korea.ac.kr>
2025-12-16 00:55:10 +0900

    fix:Config Annotations

--- src/main/java/com/example/GooRoomBe/GooRoomBeApplication.java
+++ src/main/java/com/example/GooRoomBe/GooRoomBeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAsync
 @SpringBootApplication
-@EnableNeo4jRepositories
 public class GooRoomBeApplication {
 
 	public static void main(String[] args) {
